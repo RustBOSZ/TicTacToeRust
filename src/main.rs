19f9dc57205a529c88b5472bb82787b924e15d7f
bogusz <bogusz2005@gmail.com>
2023-06-11 01:35:05 +0200

    jep

---
+++ src/main.rs
@@ -0,0 +1,129 @@
+#[macro_use] extern crate rocket;
+
+use std::sync::{Arc, Mutex};
+use rocket::{State, Shutdown};
+use rocket::fs::{relative, FileServer};
+use rocket::form::Form;
+use rocket::futures::TryFutureExt;
+use rocket::http::hyper::body::Buf;
+use rocket::log::private::logger;
+use rocket::response::stream::{EventStream, Event};
+use rocket::serde::{Serialize, Deserialize};
+use rocket::tokio::sync::broadcast::{channel, Sender, error::RecvError};
+use rocket::tokio::select;
+use rocket::tokio::sync::RwLock;
+
+
+#[derive(Debug, Clone, FromForm, Serialize, Deserialize)]
+#[serde(crate = "rocket::serde")]
+struct Message {
+
+    pub player: bool,
+    pub num: i32
+
+}
+
+fn sAdd(res:i32, num: u32) -> i32 {
+
+   if (res & 2_i32.pow(num)) != 2_i32.pow(num) {
+       return  res + 2_i32.pow(num);
+    }else {
+       return  res;
+    }
+
+}
+
+#[derive(Debug, Clone,Copy, FromForm, Serialize, Deserialize)]
+#[serde(crate = "rocket::serde")]
+struct MainState {
+    pub BX: i32,
+    pub BO: i32,
+    pub prime: bool,
+}
+
+static mut x: i32 = 0;
+static mut o: i32 = 0;
+static mut p: bool = true;
+
+/// Returns an infinite stream of server-sent events. Each event is a message
+/// pulled from a broadcast queue sent by the post handler.
+#[get("/events")]
+async fn events(queue: &State<Sender<MainState>>, mut end: Shutdown) -> EventStream![] {
+    let mut rx = queue.subscribe();
+    EventStream! {
+        loop {
+            let msg = select! {
+                msg = rx.recv() => match msg {
+                    Ok(msg) => msg,
+                    Err(RecvError::Closed) => break,
+                    Err(RecvError::Lagged(_)) => continue,
+                },
+                _ = &mut end => break,
+            };
+
+            yield Event::json(&msg);
+        }
+    }
+}
+
+
+
+/// Receive a message from a form submission and broadcast it to any receivers.
+#[post("/message", data = "<form>")]
+fn post(mut form: Form<Message>, queue: &State<Sender<MainState>>) {
+    // A send 'fails' if there are no active subscribers. That's okay.
+
+
+    unsafe {
+
+        if x<0|| o <0 {
+            x = 0;
+            o = 0;
+            p = true;
+        }
+
+        if form.num<0 {
+            x = 0;
+            o = 0;
+            p = true;
+        }else {
+            if form.player == p {
+                if form.player {
+                    x = sAdd(x,form.num as u32);
+                }else {
+                    o = sAdd(o, form.num as u32);
+                }
+                p = !p;
+            }
+        }
+
+        for win in vec![7, 56, 448, 73, 146, 292, 273, 84] {
+            if (x & win ) == win {
+                o = -1;
+                p = true;
+                println!("X:{} win:{}",x,win);
+                break;
+            }
+            else if (o & win) == win{
+                x = -1;
+                p = true;
+                println!("O: {} win: {}",o,win);
+                break;
+            }
+        }
+
+    let _res = queue.send(MainState{BX:x,BO: o,prime:p});
+
+    } // let _res = queue.send(form.into_inner());
+}
+
+
+#[launch]
+fn rocket() -> _ {
+
+
+    rocket::build()
+        .manage(channel::<MainState>(1024).0)
+        .mount("/", routes![post, events])
+        .mount("/", FileServer::from(relative!("static")))
+}
---
+++ static/script.js
@@ -0,0 +1,148 @@
+let playerField = document.querySelector("#player");
+let board = document.querySelectorAll(".cell");
+
+let statusDiv = document.getElementById('status');
+let roundDiv = document.getElementById('round');
+let re = document.getElementById('re');
+
+let hisTemplate = document.getElementById('his');
+let hisList = document.getElementById('his-list');
+
+var STATE = {
+    connected: false,
+}
+
+function addHis(text) {
+    var node = hisTemplate.content.cloneNode(true);
+    var hisDiv = node.querySelector(".hisDiv");
+    hisDiv.innerText = text;
+    hisList.appendChild(node);
+}
+function subscribe(uri) {
+    var retryTime = 1;
+
+    function connect(uri) {
+        const events = new EventSource(uri);
+
+        events.addEventListener("message", (ev) => {
+            console.log("raw data", JSON.stringify(ev.data));
+            console.log("decoded data", JSON.stringify(JSON.parse(ev.data)));
+            addHis(JSON.stringify(JSON.parse(ev.data)));
+            const msg = JSON.parse(ev.data);
+            if (!"BX" in msg || !"BO" in msg || !"prime" in msg) return;
+            setChar(msg.BX<0?0:msg.BX,msg.BO<0?0:msg.BO)
+            if (msg.BO<0){
+                roundDiv.innerText = "won : X";
+            }else if (msg.BX<0){
+                roundDiv.innerText = "won : O";
+            }else{
+                roundDiv.innerText = "round : " + (msg.prime ? "X" : "O");
+            }
+
+        });
+
+        events.addEventListener("open", () => {
+            setConnectedStatus(true);
+            console.log(`connected to event stream at ${uri}`);
+            retryTime = 1;
+            sent(-1);
+        });
+
+        events.addEventListener("error", () => {
+            setConnectedStatus(false);
+            events.close();
+
+            let timeout = retryTime;
+            retryTime = Math.min(64, retryTime * 2);
+            console.log(`connection lost. attempting to reconnect in ${timeout}s`);
+            setTimeout(() => connect(uri), (() => timeout * 1000)());
+        });
+    }
+
+    connect(uri);
+}
+
+function setConnectedStatus(status) {
+    STATE.connected = status;
+    statusDiv.className = (status) ? "connected" : "reconnecting";
+}
+
+
+function setChar(numX,numO) {
+
+    for (let i = 0; i < 9; i++) {
+        board.item(i).innerText = "";
+
+        if((numX & Math.pow(2,i)) === Math.pow(2,i)){
+            board.item(i).innerText = "X";
+        }
+        if((numO & Math.pow(2,i)) === Math.pow(2,i)){
+            board.item(i).innerText = "O";
+        }
+    }
+}
+
+// static public ArrayList<Boolean> toBooleanList(int res){
+//
+//     ArrayList<Boolean> tmp = new ArrayList<>();
+//     int ile = 0;
+//
+//     while (Math.pow(2,ile) <= res){
+//         ile++;
+//     }
+//     for (int i = 0; i < ile; i++) {
+//         if((res & (int) Math.pow(2,i)) == Math.pow(2,i)){
+//             tmp.add(true);
+//         }else {
+//             tmp.add(false);
+//         }
+//     }
+//     return tmp;
+// }
+
+// static public Integer add(Integer res,Integer index){
+//     return ((res & (int) Math.pow(2,index)) != Math.pow(2,index)) ? (int)(res + Math.pow(2,index)) : res;
+// }
+
+function sAdd(res,num) {
+
+    return ((res &  Math.pow(2,num)) !== Math.pow(2,num)) ? res + Math.pow(2,num) : res;
+
+}
+
+function sent(num) {
+    const player = playerField.value === "x";
+    // if (!player || !num) return;
+
+    if (STATE.connected) {
+        fetch("/message", {
+            method: "POST",
+            body: new URLSearchParams({ player , num }),
+        }).then((response) => {
+            if (response.ok) {
+
+            }
+        });
+    }
+}
+
+function init() {
+
+    for (let i = 0; i < board.length; i++) {
+        board.item(i).addEventListener("click",()=>{
+
+            sent(i);
+
+        })
+    }
+    re.addEventListener("click",()=>{
+
+        sent(-1);
+
+    })
+
+    subscribe("/events");
+}
+
+init();
+
